68019ad0a7be47eeb730c9c4a20a3554b2463b97
谭建明 <jianming.tan@hualongdata.com>
2021-03-19 02:27:08 +0800

    s

--- flink/cqph/cqph.py
+++ flink/cqph/cqph.py
@@ -1,12 +1,47 @@
+from pyflink.table import  StreamTableEnvironment,EnvironmentSettings
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.common.serialization import JsonRowDeserializationSchema,SimpleStringEncoder
-from pyflink.datastream.connectors import FlinkKafkaConsumer,StreamingFileSink
 
-s_env = StreamExecutionEnvironment.get_execution_environment()
-s_env.set_parallelism(1)
-src=s_env.add_source(FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev",deserialization_schema=JsonRowDeserializationSchema(),properties={"bootstrap.servers":"10.100.1.16:9192","group.id":"cqph",}))
-src.add_sink(StreamingFileSink
-    .for_row_format('/tmp/output', SimpleStringEncoder())
-    .build())
 
-s_env.execute("test")
+env=StreamExecutionEnvironment.get_execution_environment()
+env_setting=EnvironmentSettings.new_instance().in_streaming_mode().use_blink_planner().build()
+st_env=StreamTableEnvironment.create(env,env_setting)
+
+source_sql="""
+CREATE TEMPORARY TABLE kafka_source (
+  app String,
+  busi String,
+  `date` BIGINT,
+  ip String
+) WITH (
+ 'connector' = 'kafka',
+ 'topic' = 'ULS-BUSI-LOG-dev',
+ 'properties.bootstrap.servers' = '10.100.1.16:9192',
+ 'properties.group.id' = 'testGroup',
+ 'scan.startup.mode' = 'earliest-offset',
+ 'format' = 'json'  
+)
+"""
+target_sql="""
+CREATE  TEMPORARY TABLE result_count(
+cnt BIGINT,
+url STRING,
+PRIMARY KEY(ind) NOT ENFORCED
+)
+WITH
+(
+'connector'='jdbc',
+'url'='jdbc:mysql://192.168.251.94:3306/hand_in',
+'table-name' = 'result_count',
+'username'='root',
+'password'='hl.Data2018'
+)
+"""
+
+query_sql="""
+SELECT ip,COUNT(1) AS CNT
+FROM kafka_source
+GROUP BY ip
+"""
+
+st_env.execute_sql(source_sql)
+st_env.execute_sql(query_sql)
