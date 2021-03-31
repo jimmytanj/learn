2b937efb790c6b31234ae9c9a8d2508cc0dc63ec
谭建明 <jianming.tan@hualongdata.com>
2021-03-31 22:01:48 +0800

    read kafka data

--- flink/cqph/cqph.py
+++ flink/cqph/cqph.py
@@ -1,12 +1,15 @@
+from pyflink.common.serialization import SimpleStringSchema, SimpleStringEncoder
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.common.serialization import JsonRowDeserializationSchema,SimpleStringEncoder
-from pyflink.datastream.connectors import FlinkKafkaConsumer,StreamingFileSink
+from pyflink.datastream.connectors import FlinkKafkaConsumer, StreamingFileSink
 
 s_env = StreamExecutionEnvironment.get_execution_environment()
 s_env.set_parallelism(1)
-src=s_env.add_source(FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev",deserialization_schema=JsonRowDeserializationSchema(),properties={"bootstrap.servers":"10.100.1.16:9192","group.id":"cqph",}))
+fkc=FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev", deserialization_schema=SimpleStringSchema(),
+                                          properties={"bootstrap.servers": "10.100.1.16:9192", "group.id": "cqph"})
+fkc.set_start_from_earliest()
+src = s_env.add_source(fkc)
 src.add_sink(StreamingFileSink
-    .for_row_format('/tmp/output', SimpleStringEncoder())
-    .build())
+             .for_row_format('/tmp/output', SimpleStringEncoder())
+             .build())
 
 s_env.execute("test")
