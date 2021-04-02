4141ab511c0fd4120374a95c92676f752e6e8bcb
谭建明 <jianming.tan@hualongdata.com>
2021-04-02 21:40:44 +0800

    阿斯顿

--- flink/cqph/cqph.py
+++ flink/cqph/cqph.py
@@ -1,14 +1,17 @@
+import json
+
 from pyflink.common.serialization import SimpleStringSchema, SimpleStringEncoder
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import FlinkKafkaConsumer, StreamingFileSink
 
 s_env = StreamExecutionEnvironment.get_execution_environment()
 s_env.set_parallelism(1)
-fkc=FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev", deserialization_schema=SimpleStringSchema(),
-                                          properties={"bootstrap.servers": "10.100.1.16:9192", "group.id": "cqph"})
+fkc = FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev", deserialization_schema=SimpleStringSchema(),
+                         properties={"bootstrap.servers": "10.100.1.16:9192", "group.id": "cqph"})
 fkc.set_start_from_earliest()
 src = s_env.add_source(fkc)
-src.add_sink(StreamingFileSink
+app = src.map(lambda x: json.loads(x))
+app.add_sink(StreamingFileSink
              .for_row_format('/tmp/output', SimpleStringEncoder())
              .build())
 
