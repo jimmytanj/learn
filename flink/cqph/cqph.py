d735e574aab37d0fe4a41fb262a8af7bcc94f505
谭建明 <jianming.tan@hualongdata.com>
2021-04-05 13:05:43 +0800

    add files

--- flink/cqph/cqph.py
+++ flink/cqph/cqph.py
@@ -4,7 +4,11 @@ from pyflink.common.serialization import SimpleStringSchema, SimpleStringEncoder
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import FlinkKafkaConsumer, StreamingFileSink
 from pyflink.common.typeinfo import Types
-import json
+from pyflink.datastream.functions import MapFunction
+
+
+
+
 
 s_env = StreamExecutionEnvironment.get_execution_environment()
 s_env.set_parallelism(1)
@@ -13,13 +17,13 @@ ti = Types.ROW_NAMED(["app", 'busi', 'date', 'ip'], [Types.STRING(), Types.STRIN
 builder = JsonRowDeserializationSchema.builder()
 builder.type_info(ti)
 jrds = builder.ignore_parse_errors().build()
-fkc = FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev", deserialization_schema=SimpleStringSchema(),
+fkc = FlinkKafkaConsumer(topics="ULS-BUSI-LOG-dev", deserialization_schema=jrds,
                          properties={"bootstrap.servers": "10.100.1.16:9192", "group.id": "123",
                                      "auto.offset.reset": "earliest"})
 fkc.set_start_from_earliest()
-src = s_env.add_source(fkc).map(lambda x:x,Types.STRING())
+src = s_env.add_source(fkc).map(lambda x:x.get("values"))
 src.add_sink(StreamingFileSink
-             .for_row_format('C:\\tmp\\output', SimpleStringEncoder())
+             .for_row_format('C:\\tmp\\pyoutput', SimpleStringEncoder())
              .build())
 ##
 s_env.execute("123")
